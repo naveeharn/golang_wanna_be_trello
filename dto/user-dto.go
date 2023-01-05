9ffb6efa0b5eb53e1e73df244ca4b851234ee0f7
naveeharn <naveeharn@hotmail.com>
2023-01-05 21:47:57 +0700

    update dto struct

---
+++ dto/user-dto.go
@@ -0,0 +1,18 @@
+package dto
+
+import (
+	"time"
+)
+
+type UserCreateDTO struct {
+	FirstName   string    `json:"firstName" binding:"required,alpha" validate:"min:1;max:128"`
+	LastName    string    `json:"lastName" binding:"required,alpha" validate:"min:1;max:128"`
+	Email       string    `json:"email" binding:"required,email"`
+	Password    string    `json:"password" binding:"required" validate:"min:8"`
+	BirthDate   time.Time `json:"birthDate" binding:"required" time_format:"2006-02-01"`
+	Phone       string    `json:"phone" binding:"required,e164"`
+	CountryCode string    `json:"countryCode" binding:"required,iso3166_1_alpha2"`
+}
+
+type UserUpdateDTO struct {
+}
