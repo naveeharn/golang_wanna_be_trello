6f706f377745c5240613889ff702a0198c792091
naveeharn <naveeharn@hotmail.com>
2023-01-05 21:51:38 +0700

    update authentication router

--- main.go
+++ main.go
@@ -6,11 +6,28 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/naveeharn/golang_wanna_be_trello/config"
+	"github.com/naveeharn/golang_wanna_be_trello/controller"
+	"github.com/naveeharn/golang_wanna_be_trello/middleware"
+	"github.com/naveeharn/golang_wanna_be_trello/repository"
+	"github.com/naveeharn/golang_wanna_be_trello/service"
 	"gorm.io/gorm"
 )
 
 var (
+	// gorm database
 	db *gorm.DB = config.SetupDatabaseConnection()
+
+	// repositories
+	userRepository repository.UserRepository = repository.NewUserRepository(db)
+
+	// services
+	authService service.AuthService = service.NewAuthService(userRepository)
+	userService service.UserService = service.NewUserService(userRepository)
+	jwtService  service.JWTService  = service.NewJWTService()
+
+	// controllers
+	authController controller.AuthController = controller.NewAuthController(authService, jwtService)
+	userController controller.UserController = controller.NewUserController(userService)
 )
 
 func main() {
@@ -22,6 +39,11 @@ func main() {
 	routers.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{"message": "hello"})
 	})
+
+	authRoutes := routers.Group("api/auth")
+	{
+		authRoutes.POST("/register", middleware.AuthorizeJWT(jwtService), authController.Register)
+	}
 
 	routers.Run(":4011")
 
