e50f22601fe83fb878d8d27f05a22a9dc34d2118
naveeharn <naveeharn@hotmail.com>
2023-01-05 14:13:07 +0700

    update User tags

--- entity/user.go
+++ entity/user.go
@@ -3,14 +3,14 @@ package entity
 import "time"
 
 type User struct {
-	Id          string    `json:"id" gorm:"primaryKey"`
+	Id          string    `json:"id" gorm:"primaryKey;type:varchar(24)"`
 	FirstName   string    `json:"firstName" gorm:"type:varchar(64)"`
 	LastName    string    `json:"lastName" gorm:"type:varchar(64)"`
 	Email       string    `json:"email" gorm:"not null;uniqueIndex;type:varchar(128)"`
-	Password    string    `json:"-" gorm:"->;<-;not null"`
+	Password    string    `json:"-" gorm:"->;<-;not null;type:varchar(128)"`
 	BirthDate   time.Time `json:"birthDate"`
-	Phone       string    `json:"phone" binding:"required,e164"`
-	CountryCode string    `json:"countryCode" binding:"required,iso3166_1_alpha2"`
+	Phone       string    `json:"phone" gorm:"type:varchar(12)"`
+	CountryCode string    `json:"countryCode" gorm:"type:varchar(3)"`
 	CreatedAt   time.Time `json:"createdAt"`
 	UpdatedAt   time.Time `json:"updatedAt"`
 	AccessToken string    `json:"accessToken,omitempty" gorm:"-"`
