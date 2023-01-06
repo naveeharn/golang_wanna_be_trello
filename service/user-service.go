ba26aaf799bd406ab8ee0cb17de975cef600276e
naveeharn <naveeharn@hotmail.com>
2023-01-06 21:31:59 +0700

    add GetUserById, Update, ResetPassword complete

--- service/user-service.go
+++ service/user-service.go
@@ -1,6 +1,9 @@
 package service
 
 import (
+	"fmt"
+
+	"github.com/mashingan/smapping"
 	"github.com/naveeharn/golang_wanna_be_trello/dto"
 	"github.com/naveeharn/golang_wanna_be_trello/entity"
 	"github.com/naveeharn/golang_wanna_be_trello/repository"
@@ -9,8 +12,8 @@ import (
 type UserService interface {
 	Update(user dto.UserUpdateDTO) (entity.User, error)
 	GetUserById(userId string) (entity.User, error)
+	ResetPassword(user dto.UserResetPasswordDTO) (entity.User, error)
 }
-
 type userService struct {
 	userRepository repository.UserRepository
 }
@@ -22,9 +25,49 @@ func NewUserService(userRepository repository.UserRepository) UserService {
 }
 
 func (service *userService) GetUserById(userId string) (entity.User, error) {
-	panic("unimplemented")
+	user, err := service.userRepository.GetUserById(userId)
+	return user, err
+}
+
+func (service *userService) ResetPassword(user dto.UserResetPasswordDTO) (entity.User, error) {
+	if user.Id == "" {
+		return entity.User{}, fmt.Errorf("User.Id doesn't exists")
+	}
+
+	userBeforeUpdate, err := service.userRepository.GetUserById(user.Id)
+	if err != nil {
+		return entity.User{}, err
+	}
+	if !comparePassword(userBeforeUpdate.Password, user.OldPassword) {
+		return entity.User{}, fmt.Errorf("password of user is not correct")
+	}
+	userBeforeUpdate.Password = user.NewPassword
+	updatedUser, err := service.userRepository.ResetPassword(userBeforeUpdate)
+	if err != nil {
+		return entity.User{}, err
+	}
+	return updatedUser, err
 }
 
 func (service *userService) Update(user dto.UserUpdateDTO) (entity.User, error) {
-	panic("unimplemented")
+	if user.Id == "" {
+		return entity.User{}, fmt.Errorf("User.Id doesn't exists")
+	}
+
+	userBeforeUpdate, err := service.userRepository.GetUserById(user.Id)
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	err = smapping.FillStruct(&userBeforeUpdate, smapping.MapFields(&user))
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	updatedUser, err := service.userRepository.UpdateUser(userBeforeUpdate)
+	if err != nil {
+		return entity.User{}, err
+	}
+
+	return updatedUser, nil
 }
