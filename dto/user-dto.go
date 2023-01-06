d5b42a4d6d4cd5b27894cad578966b1e2ce9ed92
naveeharn <naveeharn@hotmail.com>
2023-01-06 21:32:25 +0700

    update user DTO

--- dto/user-dto.go
+++ dto/user-dto.go
@@ -15,4 +15,16 @@ type UserCreateDTO struct {
 }
 
 type UserUpdateDTO struct {
+	Id          string    `json:"-"`
+	FirstName   string    `json:"firstName" binding:"required,alpha" validate:"min:1;max:128"`
+	LastName    string    `json:"lastName" binding:"required,alpha" validate:"min:1;max:128"`
+	BirthDate   time.Time `json:"birthDate" binding:"required" time_format:"2006-02-01"`
+	Phone       string    `json:"phone" binding:"required,e164"`
+	CountryCode string    `json:"countryCode" binding:"required,iso3166_1_alpha2"`
+}
+
+type UserResetPasswordDTO struct {
+	Id          string `json:"-"`
+	OldPassword string `json:"oldPassword" binding:"required"`
+	NewPassword string `json:"newPassword" binding:"required,min=3"`
 }
