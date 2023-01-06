6f78265834466c1f88ea654c5b7a25daaf84d795
naveeharn <naveeharn@hotmail.com>
2023-01-06 21:34:51 +0700

    update user route

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/naveeharn/golang_wanna_be_trello/config"
 	"github.com/naveeharn/golang_wanna_be_trello/controller"
+	"github.com/naveeharn/golang_wanna_be_trello/middleware"
 	"github.com/naveeharn/golang_wanna_be_trello/repository"
 	"github.com/naveeharn/golang_wanna_be_trello/service"
 	"gorm.io/gorm"
@@ -43,6 +44,15 @@ func main() {
 	{
 		authRoutes.POST("/register", authController.Register)
 		authRoutes.POST("/login", authController.Login)
+	}
+
+	userRoutes := routers.Group("api/user")
+	{
+		userRoutes.GET("/", middleware.AuthorizeJWT(jwtService, userService), userController.Profile)
+		userRoutes.GET("/:id", middleware.AuthorizeJWT(jwtService, userService), userController.GetUserById)
+		userRoutes.PUT("/", middleware.AuthorizeJWT(jwtService, userService), userController.UpdateUser)
+		userRoutes.PUT("/reset-password", middleware.AuthorizeJWT(jwtService, userService), userController.ResetPassword)
+
 	}
 
 	routers.Run(":4011")
